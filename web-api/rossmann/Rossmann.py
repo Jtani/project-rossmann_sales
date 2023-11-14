a325977ac3b5f9f5955455f9b223807d778953c9
Tani <joojitani@gmail.com>
2023-11-14 13:36:12 -0300

    update

--- web-api/rossmann/Rossmann.py
+++ web-api/rossmann/Rossmann.py
@@ -8,12 +8,12 @@ import os
 
 class Rossmann( object ):
     def __init__( self ):
-        self.home_path='C:/Users/jooji/datascience/repository/Rossmann-Sales-Prediction/'
-        self.competition_distance_scaler   = pickle.load( open( self.home_path + 'parameter/competition_distance_scaler.pkl', 'rb') )
-        self.competition_time_month_scaler = pickle.load( open( self.home_path + 'parameter/competition_time_month_scaler.pkl', 'rb') )
-        self.promo_time_week_scaler        = pickle.load( open( self.home_path + 'parameter/promo_time_week_scaler.pkl', 'rb') )
-        self.year_scaler                   = pickle.load( open( self.home_path + 'parameter/year_scaler.pkl', 'rb') )
-        self.store_type_scaler             = pickle.load( open( self.home_path + 'parameter/store_type_scaler.pkl', 'rb') )
+        
+        self.competition_distance_scaler   = pickle.load( open( 'parameter/competition_distance_scaler.pkl', 'rb') )
+        self.competition_time_month_scaler = pickle.load( open( 'parameter/competition_time_month_scaler.pkl', 'rb') )
+        self.promo_time_week_scaler        = pickle.load( open( 'parameter/promo_time_week_scaler.pkl', 'rb') )
+        self.year_scaler                   = pickle.load( open( 'parameter/year_scaler.pkl', 'rb') )
+        self.store_type_scaler             = pickle.load( open( 'parameter/store_type_scaler.pkl', 'rb') )
         
         
     def data_cleaning( self, df1 ): 
